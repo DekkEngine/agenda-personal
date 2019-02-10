a3a46a9bf9dd3a27f31cc5e771aadc9868a939e1
dekk <dekkservidores@gmail.com>
2019-02-10 10:27:20 -0500

    First Commit

---
+++ src/components/Formulario.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+class Formulario extends Component {
+    render() { 
+        return ( 
+            <form>
+                <h2>Agenda de gastos</h2>
+            </form>
+         )
+    }
+}
+ 
+export default Formulario;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return(
+            <header className="header" >
+                <h1>Finanzas Jonathan jacobo Romero</h1>
+            </header>
+        )
+    }
+}
+
+export default Header;
